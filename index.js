0a00fb67598d14d437b11ea577712f7b92cfe6b3
SagynbekovErnis <121120339+SagynbekovErnis@users.noreply.github.com>
2023-02-21 22:18:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const burger = document.querySelector(".burger");
-const burger_menu = document.querySelector(".burger_menu");
+const b = document.querySelector('.burger__conteiner');
+const d = document.querySelector('.li');
+const i = document.querySelector('.img')
 
-burger.addEventListener("click", () => {
-    burger_menu.classList.toggle("active")
+b.addEventListener('click', () => {
+   b.classList.toggle('burger_active');
+   d.classList.toggle('list-active');
+   b.classList.toggle('active');
+   i.classList.toggle('img-active');
 })
